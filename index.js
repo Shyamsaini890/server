0575b9c2a958a99793e2d3bf9634170cf269b3ed
Shyamsaini890 <Shyamsundersaini71@gmail.com>
2023-09-28 19:14:29 +0530

    Added error handling

--- index.js
+++ index.js
@@ -9,28 +9,54 @@ const socketIdToEmailMap = new Map();
 
 io.on("connection", (socket) => {
   console.log("Socket connected", socket.id);
+
   socket.on("room:join", (data) => {
-    const { email, room } = data;
-    emailToSocketIdMap.set(email, socket.id);
-    socketIdToEmailMap.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
-    socket.join(room);
-    io.to(socket.id).emit("room:join", data);
+    try {
+      const { email, room } = data;
+      emailToSocketIdMap.set(email, socket.id);
+      socketIdToEmailMap.set(socket.id, email);
+      io.to(room).emit("user:joined", { email, id: socket.id });
+      socket.join(room);
+      io.to(socket.id).emit("room:join", data);
+    } catch (error) {
+      console.error("Error handling 'room:join' event:", error);
+      // You can optionally emit an error event or take other error-handling actions here
+    }
   });
 
   socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incomming:call", { from: socket.id, offer });
+    try {
+      io.to(to).emit("incoming:call", { from: socket.id, offer });
+    } catch (error) {
+      console.error("Error handling 'user:call' event:", error);
+      // You can optionally emit an error event or take other error-handling actions here
+    }
   });
 
   socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
+    try {
+      io.to(to).emit("call:accepted", { from: socket.id, ans });
+    } catch (error) {
+      console.error("Error handling 'call:accepted' event:", error);
+      // You can optionally emit an error event or take other error-handling actions here
+    }
   });
 
   socket.on("peer:nego:needed", ({ to, offer }) => {
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+    try {
+      io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+    } catch (error) {
+      console.error("Error handling 'peer:nego:needed' event:", error);
+      // You can optionally emit an error event or take other error-handling actions here
+    }
   });
 
   socket.on("peer:nego:done", ({ to, ans }) => {
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+    try {
+      io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+    } catch (error) {
+      console.error("Error handling 'peer:nego:done' event:", error);
+      // You can optionally emit an error event or take other error-handling actions here
+    }
   });
 });
